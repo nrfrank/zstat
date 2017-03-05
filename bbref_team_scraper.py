b4f10148394a60a7d0ce238e84fa4373f089e28a
nrfrank <laurenmcquillan@Banana-Pants.local>
2017-03-05 14:04:33 -0600

    clean up
    Clean up bbref_team_scraper. Simplify some methods, add error checking,
    and expand a bit.

--- bbref_team_scraper.py
+++ bbref_team_scraper.py
@@ -28,6 +28,22 @@ def getWL(soup):
     losses = int(re.sub('[^0-9]', '', record[1]))
     return((wins, losses))
 
+def getArena(soup):
+    arenaInfo = soup.body.find(text = 'Arena:')
+    arena = arenaInfo.next_element.encode("utf-8").strip()
+    return(arena)
+
+def getAttend(soup):
+    attendInfo = soup.body.find(text = 'Attendance:').next_element.encode('utf-8').strip()
+    attend_txt = attendInfo.split(' ')[0].split(',')
+    attend = 0.0
+    N_DECS = len(attend_txt)
+    for i, value in enumerate(attend_txt):
+        power = N_DECS - i - 1
+        attend += float(value)*1000.0**power
+
+    return(attend)
+
 def getAstRt(soup):
     tbl = soup.find('table', {"id": "team_and_opponent"})
     data = tbl.find_all('tr', {'class': None})[0]
@@ -42,23 +58,44 @@ def getTmAdvanced(soup):
     team_stats = (soup
                   .find('table', {'id': 'team_misc'})
                   .find_all('tr', {'class': None})[0])
-    ortg = convertStatSafe(team_stats.find('td', {'data-stat': 'off_rtg'}).text)
-    drtg = convertStatSafe(team_stats.find('td', {'data-stat': 'def_rtg'}).text)
-    pace = convertStatSafe(team_stats.find('td', {'data-stat': 'pace'}).text)
-    ftar = convertStatSafe(team_stats.find('td', {'data-stat': 'fta_per_fga_pct'}).text)
-    fg3ar = convertStatSafe(team_stats.find('td', {'data-stat': 'fg3a_per_fga_pct'}).text)
-    efgp = convertStatSafe(team_stats.find('td', {'data-stat': 'efg_pct'}).text)
-    tovp = convertStatSafe(team_stats.find('td', {'data-stat': 'tov_pct'}).text)
-    orbp = convertStatSafe(team_stats.find('td', {'data-stat': 'orb_pct'}).text)
-    ftmr = convertStatSafe(team_stats.find('td', {'data-stat': 'ft_rate'}).text)
-    oefgp = convertStatSafe(team_stats.find('td', {'data-stat': 'opp_efg_pct'}).text)
-    otovp = convertStatSafe(team_stats.find('td', {'data-stat': 'opp_tov_pct'}).text)
-    drbp = convertStatSafe(team_stats.find('td', {'data-stat': 'drb_pct'}).text)
-    oftmr = convertStatSafe(team_stats.find('td', {'data-stat': 'opp_ft_rate'}).text)
-    return([ortg, drtg, pace, ftar, fg3ar, efgp, tovp, orbp, ftmr, oefgp, otovp, drbp, oftmr])
+    tds = team_stats.find_all('td')
+    data_adv = [convertStatSafe(td.text) for td in tds[0:18]]
+    astRt = getAstRt(soup)
+    return(data_adv + [astRt])
+    # ortg = convertStatSafe(team_stats.find('td', {'data-stat': 'off_rtg'}).text)
+    # drtg = convertStatSafe(team_stats.find('td', {'data-stat': 'def_rtg'}).text)
+    # pace = convertStatSafe(team_stats.find('td', {'data-stat': 'pace'}).text)
+    # ftar = convertStatSafe(team_stats.find('td', {'data-stat': 'fta_per_fga_pct'}).text)
+    # fg3ar = convertStatSafe(team_stats.find('td', {'data-stat': 'fg3a_per_fga_pct'}).text)
+    # efgp = convertStatSafe(team_stats.find('td', {'data-stat': 'efg_pct'}).text)
+    # tovp = convertStatSafe(team_stats.find('td', {'data-stat': 'tov_pct'}).text)
+    # orbp = convertStatSafe(team_stats.find('td', {'data-stat': 'orb_pct'}).text)
+    # ftmr = convertStatSafe(team_stats.find('td', {'data-stat': 'ft_rate'}).text)
+    # oefgp = convertStatSafe(team_stats.find('td', {'data-stat': 'opp_efg_pct'}).text)
+    # otovp = convertStatSafe(team_stats.find('td', {'data-stat': 'opp_tov_pct'}).text)
+    # drbp = convertStatSafe(team_stats.find('td', {'data-stat': 'drb_pct'}).text)
+    # oftmr = convertStatSafe(team_stats.find('td', {'data-stat': 'opp_ft_rate'}).text)
+    # return([ortg, drtg, pace, ftar, fg3ar, efgp, tovp, orbp, ftmr, oefgp, otovp, drbp, oftmr])
 
-def getTmBasic(soup, type = 'tot'):
-    type_dict = {
+def getTmBasic(soup, stat_code = 0):
+    team_stats = (soup
+                  .find('table', {'id': 'team_and_opponent'})
+                  .find_all('tr', {'class': None})[stat_code]
+                  )
+    tds = team_stats.find_all('td')
+    dataList = [convertStatSafe(td.text.strip("%")) for td in tds]
+    return(dataList)
+
+def getTmInfo(soup):
+    cf, cl = getCoach(soup)
+    w, l = getWL(soup)
+    arena = getArena(soup)
+    attend = getAttend(soup)
+    data = [cl, cf, w, l, arena, attend]
+    return(data)
+
+def team_scraper(tm, yr, stat = "all"):
+    stat_dict = {
         'tot': 0,
         'pg': 1,
         'rk': 2,
@@ -68,72 +105,38 @@ def getTmBasic(soup, type = 'tot'):
         'ork': 6,
         'oyoy': 7
     }
-    type_code = type_dict[type]
-    team_stats = (soup
-                  .find('table', {'id': 'team_and_opponent'})
-                  .find_all('tr', {'class': None})[type_code]
-                  )
-    g = convertStatSafe(team_stats.find('td', {'data-stat': 'g'}).text)
-    mp = convertStatSafe(team_stats.find('td', {'data-stat': 'mp'}).text)
-    fg = convertStatSafe(team_stats.find('td', {'data-stat': 'fg'}).text)
-    fga = convertStatSafe(team_stats.find('td', {'data-stat': 'fga'}).text)
-    fgp = convertStatSafe(team_stats.find('td', {'data-stat': 'fg_pct'}).text)
-    fg3 = convertStatSafe(team_stats.find('td', {'data-stat': 'fg3'}).text)
-    fg3a = convertStatSafe(team_stats.find('td', {'data-stat': 'fg3a'}).text)
-    fg3p = convertStatSafe(team_stats.find('td', {'data-stat': 'fg3_pct'}).text)
-    fg2 = convertStatSafe(team_stats.find('td', {'data-stat': 'fg2'}).text)
-    fg2a = convertStatSafe(team_stats.find('td', {'data-stat': 'fg2a'}).text)
-    fg2p = convertStatSafe(team_stats.find('td', {'data-stat': 'fg2_pct'}).text)
-    ft = convertStatSafe(team_stats.find('td', {'data-stat': 'ft'}).text)
-    fta = convertStatSafe(team_stats.find('td', {'data-stat': 'fta'}).text)
-    ftp = convertStatSafe(team_stats.find('td', {'data-stat': 'ft_pct'}).text)
-    orb = convertStatSafe(team_stats.find('td', {'data-stat': 'orb'}).text)
-    drb = convertStatSafe(team_stats.find('td', {'data-stat': 'drb'}).text)
-    trb = convertStatSafe(team_stats.find('td', {'data-stat': 'trb'}).text)
-    ast = convertStatSafe(team_stats.find('td', {'data-stat': 'ast'}).text)
-    stl = convertStatSafe(team_stats.find('td', {'data-stat': 'stl'}).text)
-    blk = convertStatSafe(team_stats.find('td', {'data-stat': 'blk'}).text)
-    tov = convertStatSafe(team_stats.find('td', {'data-stat': 'tov'}).text)
-    pf = convertStatSafe(team_stats.find('td', {'data-stat': 'pf'}).text)
-    pts = convertStatSafe(team_stats.find('td', {'data-stat': 'pts'}).text)
-    dataList = [g, mp, fg, fga, fgp, fg3, fg3a, fg3p, fg2, fg2a, fg2p, ft, fta, ftp,
-                orb, drb, trb, ast, stl, blk, tov, pf, pts]
-    return(dataList)
-
-def getTmInfo(tm, yr):
     url_base = "http://www.basketball-reference.com/teams/"
     if isinstance(yr, int):
         yr = str(yr)
 
     url = url_base + tm + "/" + yr + ".html"
-    print url
     r = requests.get(url, allow_redirects=False)
     if r.status_code == requests.codes.ok:
         content = re.sub(r'(?m)^\<!--.*\n?', '', r.content)
         content = re.sub(r'(?m)^\-->.*\n?', '', content)
         soup = bs(content, 'lxml')
-        box_data = getTmBasic(soup)
-        astRt = getAstRt(soup)
-        cf, cl = getCoach(soup)
-        w, l = getWL(soup)
-        adv_data = getTmAdvanced(soup)
-        team_stats = soup.find('table', {'id': 'team_misc'}).find_all('tr', {'class': None})[0]
-        mov = float(team_stats.find('td', {'data-stat': 'mov'}).text)
-        sos = float(team_stats.find('td', {'data-stat': 'sos'}).text)
-        srs = float(team_stats.find('td', {'data-stat': 'srs'}).text)
-        arena = str(team_stats.find('td', {'data-stat': 'arena_name'}).text)
-        attend_txt = team_stats.find('td', {'data-stat': 'attendance'}).text.split(',')
-        attend = 0.0
-        N_PLACES = len(attend_txt)
-        for i, value in enumerate(attend_txt):
-            power = N_PLACES - i - 1
-            attend += float(value)*1000.0**power
+        if stat == 'all':
+            box_data = []
+            for stat, code in stat_dict.iteritems():
+                basic_data = getTmBasic(soup, stat_code = code)
+                box_data += basic_data
 
-        data = [tm, yr, cl, cf, w, l, mov, sos, srs, arena, attend] + box_data + adv_data + [astRt]
-        # data = [tm, yr, astRt]
-        return(data)
+        else:
+            box_data = getTmBasic(soup, stat_dict[stat])
+
+        info_data = getTmInfo(soup)
+        adv_data = getTmAdvanced(soup)
+        all_data = info_data + box_data + adv_data
+        return(all_data)
     else:
-        return([])
+        print("No team page found at {}".format(url))
+
+def getTmData(tm, yrs = (1950, 2016), stats = "all"):
+    if stats = 'all':
+        title = []
+    for yr in range(yrs[0], yrs[1] + 1):
+        print yr
+
 
 # year = '2012'
 # teams = ["ATL","BOS", "BRK","CHA", "CHO","CHI","CLE","DAL","DEN","DET","GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN","NJN","NOH", "NOK", "NOP","NYK", "OKC", "ORL","PHI","PHO","POR","SAC","SAS","SEA","TOR","UTA","WAS"]
@@ -160,6 +163,5 @@ def getTmInfo(tm, yr):
 
 # print getTblHeader(table)
 if __name__ == '__main__':
-    data = getTmInfo('BOS', '1950')
-    # data = getTmInfo('CHI', '2010')
+    data = getTmData('CHA')
     print data
